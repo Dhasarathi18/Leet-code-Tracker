e45f621005118ea02178eab330ea7791c307d528
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 20:01:00 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -0,0 +1,13 @@
+// Last updated: 7/16/2026, 8:01:03 PM
+#include <stdbool.h>
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+    long reverse = 0;
+    int original = x;
+    while (x > 0) {
+        reverse = reverse * 10 + (x % 10);
+        x /= 10;
+    }
+    return original == reverse;
+}
