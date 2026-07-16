00bd963402ac65e5dabf008edc461b479059ce43
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 20:01:06 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 7/16/2026, 8:01:09 PM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] {map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
