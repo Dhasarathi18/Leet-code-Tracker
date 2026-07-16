52ccd94033f12699fd20a2eeedc6287a2fbe016c
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:56:13 +0530

    Create 1029-VerticalOrderTraversalOfABinaryTree

---
+++ 1029-VerticalOrderTraversalOfABinaryTree/1029-VerticalOrderTraversalOfABinaryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 7/16/2026, 7:56:16 PM
+class Solution {
+    List<int[]> list = new ArrayList<>();
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root, 0, 0);
+        Collections.sort(list, (a, b) -> {
+            if (a[1] != b[1]) return a[1] - b[1]; 
+            if (a[0] != b[0]) return a[0] - b[0]; 
+            return a[2] - b[2];                   
+        });
+        List<List<Integer>> result = new ArrayList<>();
+        int prevCol = Integer.MIN_VALUE;
+        for (int[] node : list) {
+            int col = node[1];
+            int val = node[2];
+            if (col != prevCol) {
+                result.add(new ArrayList<>());
+                prevCol = col;
+            }
+            result.get(result.size() - 1).add(val);
+        }
+        return result;
+    }
+    void dfs(TreeNode root, int row, int col) {
+        if (root == null) return;
+        list.add(new int[]{row, col, root.val});
+        dfs(root.left, row + 1, col - 1);
+        dfs(root.right, row + 1, col + 1);
+    }
+}
