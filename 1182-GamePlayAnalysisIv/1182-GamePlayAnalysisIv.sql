1b151e58e1719fcff095b022adc069e2bd636f39
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:55:51 +0530

    Create 1182-GamePlayAnalysisIv

---
+++ 1182-GamePlayAnalysisIv/1182-GamePlayAnalysisIv.sql
@@ -0,0 +1,15 @@
+-- Last updated: 7/16/2026, 7:55:54 PM
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) b
+ON a.player_id = b.player_id
+AND a.event_date = DATE_ADD(b.first_login, INTERVAL 1 DAY);
