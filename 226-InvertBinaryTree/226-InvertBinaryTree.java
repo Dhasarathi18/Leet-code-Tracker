4dec0a64dd2a6ed47635f3454387da6e732fb09d
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:58:21 +0530

    Create 226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/226-InvertBinaryTree.java
@@ -0,0 +1,14 @@
+// Last updated: 7/16/2026, 7:58:24 PM
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
