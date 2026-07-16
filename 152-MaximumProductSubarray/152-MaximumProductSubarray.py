375a1bff1484dc883b15192b962634cf5c84be1c
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:59:02 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -0,0 +1,13 @@
+# Last updated: 7/16/2026, 7:59:05 PM
+class Solution:
+    def maxProduct(self, nums):
+        max_prod = nums[0]
+        min_prod = nums[0]
+        result = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                max_prod, min_prod = min_prod, max_prod
+            max_prod = max(nums[i], max_prod * nums[i])
+            min_prod = min(nums[i], min_prod * nums[i])
+            result = max(result, max_prod)
+        return result
