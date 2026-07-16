4a307cf58df8c2fc1c9842d4bf7f47d1896610b5
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:57:46 +0530

    Create 414-ThirdMaximumNumber

---
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.py
@@ -0,0 +1,17 @@
+# Last updated: 7/16/2026, 7:57:49 PM
+class Solution:
+    def thirdMax(self, nums):
+        first = second = third = None
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+            elif second is None or num > second:
+                third = second
+                second = num
+            elif third is None or num > third:
+                third = num
+        return first if third is None else third
