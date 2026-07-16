ccf08a5902668923858fe9d6ddfaebd47eb99b54
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:58:10 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.java
@@ -0,0 +1,23 @@
+// Last updated: 7/16/2026, 7:58:13 PM
+import java.util.*;
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
+                dq.pollLast();
+            }
+            dq.addLast(i);
+            if (i >= k - 1) {
+                result[idx++] = nums[dq.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
