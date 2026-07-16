5ceb0c497c329561682c6c144f36eaee5413c063
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:56:23 +0530

    Create 953-ReverseOnlyLetters

---
+++ 953-ReverseOnlyLetters/953-ReverseOnlyLetters.py
@@ -0,0 +1,18 @@
+# Last updated: 7/16/2026, 7:56:26 PM
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if not s[left].isalpha():
+                left += 1
+            elif not s[right].isalpha():
+                right -= 1
+            else:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
+        return "".join(s)
