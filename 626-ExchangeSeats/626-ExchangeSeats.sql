bf10445c166fc1c8a41ce34a0c4ac2de08021770
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:56:46 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/16/2026, 7:56:49 PM
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
