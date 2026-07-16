12431f8652677f76194f397f86d05658d2a2240f
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:56:31 +0530

    Create 783-SearchInABinarySearchTree

---
+++ 783-SearchInABinarySearchTree/783-SearchInABinarySearchTree.java
@@ -0,0 +1,12 @@
+// Last updated: 7/16/2026, 7:56:34 PM
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+}
