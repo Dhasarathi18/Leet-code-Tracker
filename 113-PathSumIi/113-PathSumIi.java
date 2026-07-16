0ee4c614d35b3c4a9e0c792d3ba979748cf4e116
Dhasarathi18 <24244017@drngpit.ac.in>
2026-07-16 19:59:26 +0530

    Create 113-PathSumIi

---
+++ 113-PathSumIi/113-PathSumIi.java
@@ -0,0 +1,27 @@
+// Last updated: 7/16/2026, 7:59:29 PM
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(root, targetSum, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, int targetSum, List<Integer> path, List<List<Integer>> result) {
+        if (node == null) {
+            return;
+        }
+
+        path.add(node.val);
+
+        if (node.left == null && node.right == null && targetSum == node.val) {
+            result.add(new ArrayList<>(path));
+        }
+
+        dfs(node.left, targetSum - node.val, path, result);
+        dfs(node.right, targetSum - node.val, path, result);
+
+        path.remove(path.size() - 1);
+    }
+}
+
